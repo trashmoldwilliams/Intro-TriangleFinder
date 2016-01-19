4377abf1e85decfa4232fb3af4ac3c446cab1e77
lawton browning and Will Johnson <lawton.a.browning@gmail.com>
2016-01-19 10:27:47 -0800

    final triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
-var triangle = function(triangle) {
-  return false;
-}
+var triangle = function(sideOne, sideTwo, sideThree) {
+  if (sideOne <= (sideTwo + sideThree) && sideTwo <=(sideThree + sideOne) && sideThree <= sideOne + sideTwo) {
+    if (sideOne===sideTwo && sideTwo===sideThree) {
+      return "equilateral";
+    } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo){
+      return "isosceles";
+    } else {
+      return "scalene";
+    }
+  } else {
+    return "not a triangle";
+  }
+};
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+
+    var sideOne = parseInt($("input#sideOne").val());
+    var sideTwo = parseInt($("input#sideTwo").val());
+    var sideThree = parseInt($("input#sideThree").val());
+    var result = triangle(sideOne, sideTwo, sideThree);
+
+    sideOne = parseInt($("input#sideOne").val(""));
+    sideTwo = parseInt($("input#sideTwo").val(""));
+    sideThree = parseInt($("input#sideThree").val(""));
+
+    $("#result").text(result);
+    event.preventDefault();
+  });
+});
--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,17 @@
 describe("triangle", function() {
-  it("is false that a triangle has a side of zero", function() {
-  expect(triangle(0, 1, 1)).to.equal(false);
+  it("determines if 3 sides create a valid triangle", function() {
+  expect(triangle(2, 2, 8000)).to.equal("not a triangle");
+  });
+
+  it("determines if 3 sides make an equilateral triangle",function() {
+    expect(triangle(7, 7, 7)).to.equal("equilateral")
+  });
+
+  it("determines if 3 sides make an isosceles triangle",function() {
+    expect(triangle(7, 7, 4)).to.equal("isosceles")
+  });
+
+  it("determines if 3 sides make a scalene triangle",function() {
+    expect(triangle(2, 3, 4)).to.equal("scalene")
   });
 })
